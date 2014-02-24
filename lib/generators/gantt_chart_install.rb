35c42ebf0f07b2abcec9229d78f45f4eed036e7e
Ankita Kanitkar <ankitakanitkar@gmail.com>
2014-02-25 00:49:42 +0530

    removed generators

--- lib/generators/gantt_chart_install.rb
+++
@@ -1,8 +0,0 @@
-require 'rails'
-
-class GanttChartInstallGenerator < ::Rails::Generators::Base
-
-  desc "This generator installs visual search javscripts and its dependencies"
-  source_root File.expand_path('../../../../../app/assets/javascripts', __FILE__)
-
-end
